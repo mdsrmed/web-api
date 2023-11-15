fa94abd53408127deb26f55b978e5d71943a9d4e
mdsrmed <mdsrmed@gmail.com>
2023-11-15 13:55:44 -0500

    Router setup with axum and tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+
+#[tokio::main]
+
+async fn main() {
     println!("Hello, world!");
+    server().await;
+}
+
+async fn server() {
+    let app: Router = Router::new().route("/api/test", get(test));
+
+    axum::Server::bind(&"0.0.0.0:4040".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn test() -> impl IntoResponse {
+    println!("test!");
+    (StatusCode::ACCEPTED, "hello")
 }
